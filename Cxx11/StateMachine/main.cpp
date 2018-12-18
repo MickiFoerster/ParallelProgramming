30cfc7fb66114ca98c2f8027f202e921669cbc71
Michael Foerster <Michael.Foerster@com-science.de>
2018-12-18 21:09:06 +0100

    skelton of C++11 statemachine ready

--- Cxx11/StateMachine/main.cpp
+++ Cxx11/StateMachine/main.cpp
@@ -1,13 +1,48 @@
 #include <iostream>
 #include <thread>
-
-void statemachine() {
-  std::cerr << "Hello\n";
-}
+#include <atomic>
+/*                                         -loop-
+ * Statemachine from websockets             \ /send()
+ * -> connecting -> < SUCCESS > -- yes --> open
+ *                      |                    |
+ *                      no                   close()
+ *                      |                    |
+ *                      V                    V
+ *                    closed  <----------- closing
+ */
+class StateMachine {
+  std::unique_ptr<std::thread> tid;
+  std::atomic<bool> terminateThread;
+public:
+  StateMachine();
+  ~StateMachine();
+  void endStatemachine(void);
+  void setTransitionDisconnectedConnecting(void);
+  void setTransitionConnectingOpen(short val);
+  void setTransitionOpenClosing(void);
+  void setTransitionClosingClosed(void);
+  friend void statemachineTask(StateMachine* sm);
+};
 
 int main() {
-  std::thread t{statemachine};
-
-  t.join();
+  StateMachine statemachine;
   return 0;
+}
+
+void statemachineTask(StateMachine* sm) {
+  for(;;) {
+    if (sm->terminateThread.load(std::memory_order_acquire)) {
+      break;
+    }
+  }
+}
+
+StateMachine::~StateMachine() {
+  terminateThread.store(true, std::memory_order_release);
+  tid->join();
+}
+
+StateMachine::StateMachine() {
+  terminateThread.store(false, std::memory_order_release);
+  tid = std::unique_ptr<std::thread>{new std::thread{statemachineTask, this}};
 }
